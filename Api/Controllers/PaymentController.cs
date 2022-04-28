aa006ef23404ed05317e342aaf551d71d91e22d3
pyabzz <>
2022-04-28 14:16:37 +0100

    Add Api

---
+++ Api/Controllers/PaymentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PaymentController : ControllerBase
+    {
+        private readonly ILogger<PaymentController> _logger; //todo: Think something for this
+
+        public PaymentController(ILogger<PaymentController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<int> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5)
+                .Select(index => rng.Next(-20, 55))
+                .ToArray();
+        }
+    }
+}
